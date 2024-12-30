60092397688eca7be2582a8735347d473b0d4f65
Ryan Acevedo <ryan.avocado@protonmail.com>
2024-12-29 17:39:25 -0700

    added dependency injection for getRandomNumber Controller

--- server/src/random/random.controller.ts
+++ server/src/random/random.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { RandomService } from './random.service';
 
 @Controller('random')
 export class RandomController {
+  constructor(private randomService: RandomService) {}
   @Get()
-  getRandomNumber(): { number: number } {
-    
+  getRandomNumber() {
+    return this.randomService.startNumberGeneration();
   }
 }
