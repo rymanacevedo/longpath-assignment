f99243b29a982c670cb7490690834724bf088de9
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-01-01 15:27:46 -0700

    updated websocket connection to allow connection to localhost:3000

--- server/src/number-generation/number-generation.gateway.ts
+++ server/src/number-generation/number-generation.gateway.ts
@@ -1,28 +1,59 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer }
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer }
 from '@nestjs/websockets';
-
-import {Server} from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 // TODO: https://docs.nestjs.com/security/rate-limiting#websockets
-@WebSocketGateway()
-export class NumberGenerationGateway {
+@WebSocketGateway({
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+})
+export class NumberGenerationGateway implements OnGatewayConnection, OnGatewayDisconnect{
 
 
   @WebSocketServer() io: Server;
 
-  handleConnection(client: any, ...args: any[]) {
-    const {sockets} = this.io.sockets;
+  private intervalId: NodeJS.Timeout | null = null;
+  private frequency = 1000; // Default frequency in ms
+
+  // Handle new client connection
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+    client.emit('connected', { message: 'Welcome to the WebSocket server!' });
   }
 
-  handleDisconnect(client: any) {
-
+  
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
   }
 
   @SubscribeMessage('ping')
-  handleMessage(client: any, payload: any) {
+  handleMessage(client: Socket, payload: any) {
     return {
       event: 'pong',
       data: payload
     }
   }
+
+    @SubscribeMessage('start')
+    startGeneration(client: Socket) {
+      if (this.intervalId) {
+        client.emit('error', { message: 'Generation already started' });
+        return;
+      }
+  
+      this.intervalId = setInterval(() => {
+        const randomNumber = Math.floor(Math.random() * 100);
+        const timestamp = new Date().toISOString();
+        const data = { value: randomNumber, timestamp };
+  
+        // Broadcast the random number to all connected clients
+        this.io.emit('random-number', data);
+      }, this.frequency);
+  
+      return { event: 'started', message: 'Number generation started' };
+    }
+  
 }
