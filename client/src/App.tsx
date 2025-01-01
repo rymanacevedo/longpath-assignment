3c59764333ed73544b2b28c0cb570ba292d9b621
Ryan Acevedo <ryan.avocado@protonmail.com>
2024-12-31 18:17:43 -0700

    updated cors to allow GET, PUT requests from localhost:3000 and created a start/stop button to begin polling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import io from 'socket.io-client';
+import ReactECharts from 'echarts-for-react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState<{ timestamp: string; value: number }[]>([]);
+
+  
+  const startNumberGeneration = async () => {
+    await axios.post('http://localhost:3001/random/start');
+  }
+
+  const stopNumberGeneration = async () => {
+    await axios.post('http://localhost:3001/random/stop');
+  };
+
+  const chartOptions = {
+    xAxis: { type: 'category', data: data.map((d) => d.timestamp) },
+    yAxis: { type: 'value' },
+    series: [{ data: data.map((d) => d.value), type: 'line' }],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>LongPath App</h1>
-        <p>Hello X, Good luck with the problem!</p>
-      </header>
+    <div>
+      <h1>Random Number Generator</h1>
+      <button onClick={startNumberGeneration}>Start</button> 
+      <button onClick={stopNumberGeneration}>Stop</button>
+
+      <ReactECharts option={chartOptions} />
     </div>
   );
-}
+};
 
 export default App;
