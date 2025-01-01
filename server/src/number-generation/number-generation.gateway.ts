7053671f9bf7f8ebcf0fb846f6cf73184c9b0b0a
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-01-01 15:38:05 -0700

    updated websocket connection with updateFrequency and refactored start/stop methods

--- server/src/number-generation/number-generation.gateway.ts
+++ server/src/number-generation/number-generation.gateway.ts
@@ -1,8 +1,10 @@
-import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer }
-from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-// TODO: https://docs.nestjs.com/security/rate-limiting#websockets
 @WebSocketGateway({
   cors: {
     origin: 'http://localhost:3000',
@@ -10,61 +12,50 @@ import { Server, Socket } from 'socket.io';
     credentials: true,
   },
 })
-export class NumberGenerationGateway implements OnGatewayConnection, OnGatewayDisconnect{
-
+export class NumberGenerationGateway {
   @WebSocketServer() io: Server;
 
   private intervalId: NodeJS.Timeout | null = null;
   private frequency = 1000; // Default frequency in ms
 
-  handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
-    client.emit('connected', { message: 'Welcome to the WebSocket server!' });
-  }
-
-  
-  handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
-  }
-
-  @SubscribeMessage('ping')
-  handleMessage(client: Socket, payload: any) {
-    return {
-      event: 'pong',
-      data: payload
-    }
-  }
-
-    @SubscribeMessage('start')
-    startGeneration(client: Socket) {
-      if (this.intervalId) {
-        client.emit('error', { message: 'Generation already started' });
-        return;
-      }
-  
-      this.intervalId = setInterval(() => {
-        const randomNumber = Math.floor(Math.random() * 100);
-        const timestamp = new Date().toISOString();
-        const data = { value: randomNumber, timestamp };
-  
-        this.io.emit('random-number', data);
-      }, this.frequency);
-  
-      return { event: 'started', message: 'Number generation started' };
+  startGeneration() {
+    if (this.intervalId) {
+      return;
     }
 
-    @SubscribeMessage('stop')
-    stopGeneration(client: Socket) {
-      if (!this.intervalId) {
-        client.emit('error', { message: 'Generation is not running' });
-        return;
-      }
-  
+    this.intervalId = setInterval(() => {
+      const randomNumber = Math.floor(Math.random() * 100);
+      const timestamp = new Date().toISOString();
+      const data = { value: randomNumber, timestamp };
+
+      this.io.emit('random-number', data);
+    }, this.frequency);
+  }
+
+  stopGeneration() {
+    if (this.intervalId) {
       clearInterval(this.intervalId);
       this.intervalId = null;
-  
-      return { event: 'stopped', message: 'Number generation stopped' };
     }
-  
-  
+  }
+
+  updateFrequency(newFrequency: number) {
+    this.frequency = newFrequency;
+    if (this.intervalId) {
+      this.stopGeneration();
+      this.startGeneration();
+    }
+  }
+
+  @SubscribeMessage('start')
+  handleStart() {
+    this.startGeneration();
+    return { event: 'started', message: 'Number generation started' };
+  }
+
+  @SubscribeMessage('stop')
+  handleStop() {
+    this.stopGeneration();
+    return { event: 'stopped', message: 'Number generation stopped' };
+  }
 }
