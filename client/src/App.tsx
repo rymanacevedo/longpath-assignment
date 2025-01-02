96b07ace053248cdd4fad80ca2c6b55d052cb615
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-01-02 22:10:09 +0000

    added toast messages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,19 @@ import ReactECharts from 'echarts-for-react';
 import DateTimeSelector from './components/DateTimeSelector';
 import HistoryTable from './components/HistoryTable';
 import './output.css';
+import Toast from './components/Toasty';
 
 const App = () => {
   const [data, setData] = useState<{ timestamp: string; value: number }[]>([]);
-  const [filteredData, setFilteredData] = useState<{ timestamp: string; value: number }[]>([]);
+  const [filteredData, setFilteredData] = useState<
+    { timestamp: string; value: number }[]
+  >([]);
   const [socket, setSocket] = useState<Socket | null>(null);
   const [frequency, setFrequency] = useState(1000);
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [endDate, setEndDate] = useState<Date | null>(null);
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
 
   useEffect(() => {
     const newSocket = io('http://localhost:3001', {
@@ -49,11 +54,21 @@ const App = () => {
     setFilteredData(filtered);
   }, [startDate, endDate, data]);
 
+  const handleShowToast = (message: string) => {
+    setToastMessage(message);
+    setShowToast(true);
+    setTimeout(() => {
+      setToastMessage('');
+      setShowToast(false);
+    }, 3000)
+  }
+
   const startNumberGeneration = async () => {
     if (!socket) {
       return;
     }
     socket.emit('start');
+    handleShowToast('Successfully started number generation');
   };
 
   const stopNumberGeneration = async () => {
@@ -61,6 +76,7 @@ const App = () => {
       return;
     }
     socket.emit('stop');
+    handleShowToast('Stopped number generation');
   };
 
   const updateFrequency = async () => {
@@ -76,55 +92,59 @@ const App = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-  <div className="flex flex-col space-y-4 lg:space-y-0 lg:flex-row lg:space-x-4">
-    {/* Left Panel */}
-    <div className="flex-1">
-      {/* Chart and Controls */}
-      <h1 className="text-2xl font-bold text-center mb-4">Random Number Generator</h1>
-      <div className="flex flex-col md:flex-row md:space-x-4">
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-          onClick={startNumberGeneration}
-        >
-          Start
-        </button>
-        <button
-          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700"
-          onClick={stopNumberGeneration}
-        >
-          Stop
-        </button>
-        <input
-          className="border rounded p-2"
-          type="number"
-          value={frequency}
-          onChange={(e) => setFrequency(Number(e.target.value))}
-          placeholder="Frequency (ms)"
-        />
-        <button
-          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700"
-          onClick={updateFrequency}
-        >
-          Update
-        </button>
+    <>
+      <div className="container mx-auto p-4">
+        <div className="flex flex-col space-y-4 lg:space-y-0 lg:flex-row lg:space-x-4">
+          {/* Left Panel */}
+          <div className="flex-1">
+            {/* Chart and Controls */}
+            <h1 className="text-2xl font-bold text-center mb-4">
+              Random Number Generator
+            </h1>
+            <div className="flex flex-col md:flex-row md:space-x-4">
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+                onClick={startNumberGeneration}
+              >
+                Start
+              </button>
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700"
+                onClick={stopNumberGeneration}
+              >
+                Stop
+              </button>
+              <input
+                className="border rounded p-2"
+                type="number"
+                value={frequency}
+                onChange={(e) => setFrequency(Number(e.target.value))}
+                placeholder="Frequency (ms)"
+              />
+              <button
+                className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700"
+                onClick={updateFrequency}
+              >
+                Update
+              </button>
+            </div>
+          </div>
+
+          {/* Right Panel */}
+          <div className="flex-1">
+            <ReactECharts option={chartOptions} className="w-full h-80" />
+            <DateTimeSelector
+              startDate={startDate}
+              setStartDate={setStartDate}
+              endDate={endDate}
+              setEndDate={setEndDate}
+            />
+            <HistoryTable data={filteredData} />
+          </div>
+        </div>
       </div>
-    </div>
-
-    {/* Right Panel */}
-    <div className="flex-1">
-      <ReactECharts option={chartOptions} className="w-full h-80" />
-      <DateTimeSelector
-        startDate={startDate}
-        setStartDate={setStartDate}
-        endDate={endDate}
-        setEndDate={setEndDate}
-      />
-      <HistoryTable data={filteredData} />
-    </div>
-  </div>
-</div>
-
+      {showToast ? <Toast message={toastMessage} type={'error'} /> : null}
+    </>
   );
 };
 
---
+++ client/src/components/Toasty.tsx
@@ -0,0 +1,24 @@
+type Toasty = {
+  message: string;
+  type: 'success' | 'error' | 'info';
+};
+
+const Toast = ({ message, type = 'info' }: Toasty) => {
+  const typeClasses = {
+    success: 'bg-green-500',
+    error: 'bg-red-500',
+    info: 'bg-blue-500',
+  };
+
+  return (
+    <div
+      className={`absolute bottom-5 w-1 right-5  flex items-center p-4 text-black rounded shadow-lg ${typeClasses[type]}`}
+    >
+      <div className="flex-1">
+        <p className="text-sm">{message}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Toast;
